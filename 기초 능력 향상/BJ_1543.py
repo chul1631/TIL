076cdc9b6344f14f1926048de52802645dceca0e
chul1631 <sonky201@gmail.com>
2022-10-31 21:23:16 +0900

    기초 능력 향상 ~65번

---
+++ 기초 능력 향상/BJ_1543.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open("BJ_1543.txt")
+
+a = input()
+b = input()
+cnt = 0
+n = 0
+while n <= len(a) - len(b):
+    if a[n : n + len(b)] == b:
+        cnt += 1
+        n += len(b)
+    else:
+        n += 1
+print(cnt)
